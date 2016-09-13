b677c55b22d203c825f8c91b59837a8563c6a61b
Aaron Schif <aschif@netdevgroup.com>
2016-09-13 15:41:39 -0500

    Full tabs

--- src/game.js
+++ src/game.js
@@ -2,34 +2,34 @@
 
 
 export function Game(screen, updateFunction, renderFunction) {
-  this.update = updateFunction;
-  this.render = renderFunction;
+    this.update = updateFunction;
+    this.render = renderFunction;
 
-  // Set up buffers
-  this.frontBuffer = screen;
-  this.frontCtx = screen.getContext('2d');
-  this.backBuffer = document.createElement('canvas');
-  this.backBuffer.width = screen.width;
-  this.backBuffer.height = screen.height;
-  this.backCtx = this.backBuffer.getContext('2d');
+    // Set up buffers
+    this.frontBuffer = screen;
+    this.frontCtx = screen.getContext('2d');
+    this.backBuffer = document.createElement('canvas');
+    this.backBuffer.width = screen.width;
+    this.backBuffer.height = screen.height;
+    this.backCtx = this.backBuffer.getContext('2d');
 
-  // Start the game loop
-  this.oldTime = performance.now();
-  this.paused = false;
+    // Start the game loop
+    this.oldTime = performance.now();
+    this.paused = false;
 }
 
 Game.prototype.pause = function(flag) {
-  this.paused = (flag == true);
+    this.paused = (flag == true);
 }
 
 Game.prototype.loop = function(newTime) {
-  var game = this;
-  var elapsedTime = newTime - this.oldTime;
-  this.oldTime = newTime;
+    var game = this;
+    var elapsedTime = newTime - this.oldTime;
+    this.oldTime = newTime;
 
-  if(!this.paused) this.update(elapsedTime);
-  this.render(elapsedTime, this.frontCtx);
+    if(!this.paused) this.update(elapsedTime);
+    this.render(elapsedTime, this.frontCtx);
 
-  // Flip the back buffer
-  this.frontCtx.drawImage(this.backBuffer, 0, 0);
+    // Flip the back buffer
+    this.frontCtx.drawImage(this.backBuffer, 0, 0);
 }
--- src/player.js
+++ src/player.js
@@ -2,44 +2,77 @@
 
 const MS_PER_FRAME = 1000/8;
 
+const STATES = {
+    idle: Symbol(),
+    walking: Symbol(),
+    blinking: Symbol(),
+    jumping: Symbol(),
+}
+
 export function Player(position) {
-  this.state = "idle";
-  this.x = position.x;
-  this.y = position.y;
-  this.width  = 64;
-  this.height = 64;
-  this.spritesheet  = new Image();
-  this.spritesheet.src = encodeURI('assets/PlayerSprite2.png');
-  this.timer = 0;
-  this.frame = 0;
+    this.state = STATES.jumping;
+    this.x = position.x;
+    this.y = position.y;
+    this.width  = 64;
+    this.height = 64;
+    this.spritesheet  = new Image();
+    this.spritesheet.src = encodeURI('assets/PlayerSprite2.png');
+    this.timer = 0;
+    this.frame = 0;
+    this.sittingSprites = [{x: 64*3, y: 64}, {x: 64*0, y: 64}, {x: 64*1, y: 64}, {x: 64*2, y: 64}, {x: 64*1, y: 64}, {x: 64*0, y: 64}];
+    this.jumpingSprites = [{x: 64*3, y: 0}, {x: 64*2, y: 0}, {x: 64*1, y: 0}, {x: 64*0, y: 0}, {x: 64*1, y: 0}, {x: 64*2, y: 0}, {x: 64*3, y: 0}];
 }
 
 Player.prototype.update = function(time) {
-  switch(this.state) {
-    case "idle":
-      this.timer += time;
-      if(this.timer > MS_PER_FRAME) {
+    this.timer += time;
+    let newFrame = false;
+    if(this.timer > MS_PER_FRAME) {
         this.timer = 0;
-        this.frame += 1;
-        if(this.frame > 3) this.frame = 0;
-      }
-      break;
-    // TODO: Implement your player's update by state
-  }
+        this.frame++;
+        newFrame = true;
+    }
+    switch(this.state) {
+        case STATES.idle:
+            break;
+        case STATES.jumping: {
+            if (newFrame)
+                this.x += this.width / this.jumpingSprites.length;
+        }
+        // TODO: Implement your player's update by state
+    }
 }
 
 Player.prototype.render = function(time, ctx) {
-  switch(this.state) {
-    case "idle":
-      ctx.drawImage(
-        // image
-        this.spritesheet,
-        // source rectangle
-        this.frame * 64, 64, this.width, this.height,
-        // destination rectangle
-        this.x, this.y, this.width, this.height
-      );
-      break;
-    // TODO: Implement your player's redering according to state
-  }
+    switch(this.state) {
+        case STATES.jumping: {
+            let frame = this.frame % (this.jumpingSprites.length);
+            let {x, y} = this.jumpingSprites[frame];
+            ctx.drawImage(
+                this.spritesheet,
+                x, y, this.width, this.height,
+                this.x, this.y, this.width, this.height
+            );
+            if (this.frame === this.jumpingSprites.length) {
+                this.frame = 0;
+                this.state = STATES.idle;
+            }
+            break;
+        }
+
+        case STATES.idle:
+            // handle blinking
+            let frame = Math.min(this.frame % (this.sittingSprites.length + 20), this.sittingSprites.length);
+            frame = frame % this.sittingSprites.length;
+            let {x, y} = this.sittingSprites[frame];
+            ctx.drawImage(
+                // image
+                this.spritesheet,
+                // source rectangle
+                x, y, this.width, this.height,
+                // destination rectangle
+                this.x, this.y, this.width, this.height
+            );
+            break;
+            // TODO: Implement your player's redering according to state
+    }
 }
